f172ac7b76e3317490bbcf95b21d91ab5ca46ae2
Nayla-Sya <azizanaylasyazana@gmail.com>
2026-06-07 20:01:18 +0800

    Selesai Bagian 3: Multi-Class

---
+++ src/bagian3/perpustakaan/Buku.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bagian3.perpustakaan;
+
+/**
+ *
+ * @author Nayla
+ */
+public class Buku {
+    // Atribut: data yang dimiliki setiap buku
+ private String judul;
+    private String penulis;
+    private int tahunTerbit;
+    private boolean dipinjam;
+
+    public Buku(String judul, String penulis, int tahunTerbit) {
+        this.judul = judul;
+        this.penulis = penulis;
+        this.tahunTerbit = tahunTerbit;
+        this.dipinjam = false;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public String getPenulis() {
+        return penulis;
+    }
+
+    public int getTahunTerbit() {
+        return tahunTerbit;
+    }
+
+    public boolean isDipinjam() {
+        return dipinjam;
+    }
+
+    public void setDipinjam(boolean dipinjam) {
+        this.dipinjam = dipinjam;
+    }
+
+    public String info() {
+        String status = dipinjam ? "Dipinjam" : "Tersedia";
+        return judul + " oleh " + penulis +
+               " (" + tahunTerbit + ")" +
+               " [" + status + "]";
+    }
+
+}
